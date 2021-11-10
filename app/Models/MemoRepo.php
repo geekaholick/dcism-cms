53ca2c3e8285372d89382f1f43a5cc935b2d5231
geekaholick <33748869+geekaholick@users.noreply.github.com>
2021-11-10 15:36:08 +0800

    feat(View All Announcement Page): Added AnnouncementStore, AnnouncementType, API, API Link, AnnouncementController - created an announcement controller - created a function getAllAnnouncement inside announcement controller - created a store announcement - created an action to get all announcement - created an announcement type based on the announcement module - created an api link '/get-all-announcements' inside api.php - created a function to get the number of all announcements - created an api link '/get-all-announcements-count' inside api.php - created an action to get the announcement count - created a function 'getAnnouncementCount' inside announcement controller - created mutation for announcement count, announcement list, and error - commented out appbreadcrumb unused element - added ellipsis for announcement body if too long - added number of comments - changed to skeleton instead of spinner whenever data not yet displayed - added error handler when there are no announcements to be displayed
    IT-15

---
+++ app/Models/MemoRepo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MemoRepo extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'announcement_id',
+        'memo_path'
+    ];
+
+}
---
+++ resources/js/src/store/announcements/announcementStore.js
@@ -0,0 +1,67 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+import Api from '../../api/api'
+import * as types from './announcementTypes'
+
+const state = {
+  errors: null,
+  announcement_list: [],
+  announcement_count: 0,
+}
+
+const getters = {
+  [types.GETTER_ALL_ANNOUNCEMENTS]: state => state.announcement_list,
+}
+
+const actions = {
+  [types.ACTION_GET_ALL_ANNOUNCEMENTS](context, data) {
+    return new Promise(resolve => {
+      console.log('actions announcements')
+      console.log(data)
+      Api().get(data.items ? `/get-all-announcements?page=${data.page}&items=${data.items}`
+        : `/get-all-announcements?page=${data.page}`)
+        .then(resp => {
+          context.commit(types.MUTATION_SET_ALL_ANNOUNCEMENTS, resp.data)
+          resolve(resp.data)
+        })
+        .catch(err => {
+          context.commit(types.MUTATION_SET_ERROR, err.response.data.errors)
+          resolve(err.response.data.errors)
+        })
+    })
+  },
+  [types.ACTION_GET_ANNOUNCEMENTS_COUNT](context, data) {
+    return new Promise(resolve => {
+      Api().get('/get-all-announcements-count')
+        .then(resp => {
+          context.commit(types.MUTATION_SET_ANNOUNCEMENT_COUNT, resp.data)
+          resolve(resp.data)
+        })
+        .catch(err => {
+          context.commit(types.MUTATION_SET_ERROR, err.response.data.errors)
+          resolve(err.response.data.errors)
+        })
+    })
+  },
+}
+
+const mutations = {
+  [types.MUTATION_SET_ALL_ANNOUNCEMENTS](state, announcement_list) {
+    state.announcement_list = announcement_list
+  },
+  [types.MUTATION_SET_ERROR](state, error) {
+    state.errors = error
+  },
+  [types.MUTATION_SET_ANNOUNCEMENT_COUNT](state, count) {
+    state.announcement_count = count
+  },
+
+}
+
+export default {
+  state,
+  actions,
+  getters,
+  mutations,
+}
