26f65f910fda6a1dfde7cf8551ea3b429fd96cc9
Hansel <valkirecesa@gmail.com>
2021-11-17 17:39:10 +0800

    feat(Setup DB): Added Models and Migrations

---
+++ database/migrations/2021_11_17_093656_create_announcement_approvals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementApprovalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcement_approvals', function (Blueprint $table) {
+            $table->id();
+            $table->integer('announcement_id');
+            $table->boolean('is_approved')->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcement_approvals');
+    }
+}
