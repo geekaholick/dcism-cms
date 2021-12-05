84c5f3f6426b17089bbfa9d07ac4619c989e4dfc
bruce-vinceal <brucevinceal@gmail.com>
2021-12-06 00:16:17 +0800

    update(): added Announcement, Bookedmarked Announcement, Calendar and Memo Tab in the vertical navbar

--- resources/js/src/navigation/vertical/index.js
+++ resources/js/src/navigation/vertical/index.js
@@ -1,12 +1,28 @@
 export default [
-  {
-    title: 'Home',
-    route: 'home',
-    icon: 'HomeIcon',
-  },
-  {
-    title: 'Second Page',
-    route: 'second-page',
-    icon: 'FileIcon',
-  },
-]
+    {
+        title: "Home",
+        route: "home",
+        icon: "HomeIcon",
+    },
+    {
+        title: "Announcement",
+        route: "second-page",
+        icon: "FileIcon",
+        children: [
+            {
+                title: "Bookmarked Announcement",
+                route: "second-page",
+            },
+        ],
+    },
+    {
+        title: "Department Calendar",
+        route: "second-page",
+        icon: "CalendarIcon",
+    },
+    {
+        title: "Memo",
+        route: "second-page",
+        icon: "FileTextIcon",
+    },
+];
