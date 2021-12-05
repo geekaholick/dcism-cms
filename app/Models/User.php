9cc7ca7746ec73857d68e46a1e970df6bf4607c3
Jamian Huang <91708133+nemofira09@users.noreply.github.com>
2021-12-06 07:31:46 +0800

    IT-21 commit
    Addition and updates

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,8 +28,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'remember_token'
     ];
 
     /**
--- resources/js/src/main.js
+++ resources/js/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
+export default new Vue()
 import { ToastPlugin, ModalPlugin } from 'bootstrap-vue'
 import VueCompositionAPI from '@vue/composition-api'
+import { TablePlugin } from 'bootstrap-vue'
+import { FormInputPlugin } from 'bootstrap-vue'
+import { LayoutPlugin } from 'bootstrap-vue'
+import { PaginationPlugin } from 'bootstrap-vue'
+import { FormPlugin } from 'bootstrap-vue'
+import { FormSelectPlugin } from 'bootstrap-vue'
+import { FormFilePlugin } from 'bootstrap-vue'
+import { ButtonPlugin } from 'bootstrap-vue'
+import { ImagePlugin } from 'bootstrap-vue'
+import { AlertPlugin } from 'bootstrap-vue'
 
 import router from './router'
 import store from './store'
-import App from './App.vue'
+import FacultyManagementApp from './FacultyManagementApp.vue'
+import StudentManagementApp from './StudentManagementApp.vue'
 
 // Global Components
 import './global-components'
@@ -16,6 +28,16 @@ import '@/libs/toastification'
 // BSV Plugin Registration
 Vue.use(ToastPlugin)
 Vue.use(ModalPlugin)
+Vue.use(TablePlugin)
+Vue.use(FormInputPlugin)
+Vue.use(LayoutPlugin)
+Vue.use(PaginationPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(FormFilePlugin)
+Vue.use(ButtonPlugin)
+Vue.use(ImagePlugin)
+Vue.use(AlertPlugin)
 
 // Composition API
 Vue.use(VueCompositionAPI)
@@ -28,8 +50,18 @@ require('@/assets/scss/style.scss')
 
 Vue.config.productionTip = false
 
+if(document.getElementById("FacultyManagementApp")){
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(FacultyManagementApp),
+}).$mount('#FacultyManagementApp')
+}
+
+if(document.getElementById("StudentManagementApp")){
+  new Vue({
+    router,
+    store,
+    render: h => h(StudentManagementApp),
+  }).$mount('#StudentManagementApp')
+  }
