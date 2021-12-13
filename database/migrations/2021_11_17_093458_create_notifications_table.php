415a5b7d14ed079aadd55ba91933ebe302711f8e
janalencypino <pinojanalency@gmail.com>
2021-12-13 13:07:58 +0800

    >team

--- database/migrations/2021_11_17_093458_create_notifications_table.php
+++ database/migrations/2021_11_17_093458_create_notifications_table.php
@@ -14,10 +14,11 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->integer('announcement_id');
-            $table->integer('user_id');
-            $table->boolean('have_opened')->default(false);
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use App\Http\Controllers\ApplicationController;
 */
 
 Route::get('/{any}', [ApplicationController::class, 'index'])->where('any', '.*');
+Route::get('send', 'ApplicationController@sendNotification');
+
